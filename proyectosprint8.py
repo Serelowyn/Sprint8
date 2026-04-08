82363a8b7002dd7f20cc6bf3b7e521cdbf166ce2
Memo <fretn655@gmail.com>
2026-04-08 17:12:57 -0600

    cambios en el codigo inicial, la ruta de los archivos contenia una erronea, al verificar la tuve que cambiar para poder leerlos y asi mostrar las primeras lineas de cada data frame

--- proyectosprint8.py
+++ proyectosprint8.py
@@ -7,5 +7,10 @@ import pandas as pd
 
 """aqui se traen los dos primeros archivos excel para hacer data frame para analizarlos."""
 
-df_companies = pd.read_csv("/datasets/project_sql_result_01.csv")
-df_locations = pd.read_csv("/datasets/project_sql_result_04.csv")
+df_companies = pd.read_csv(r"C:\\Users\\sasor\\Desktop\\Tripleten\\Sprint 8\\proyecto\\moved_project_sql_result_01.csv")
+
+df_locations = pd.read_csv(r"C:\\Users\\sasor\\Desktop\\Tripleten\\Sprint 8\\proyecto\\moved_project_sql_result_04.csv")
+
+"""esto nos permite revisar que se haya cargado correctamente"""
+print(df_companies.head())
+print(df_locations.head())

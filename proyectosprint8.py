31322f0826c7991b60a8af096d77d4cd6494328c
Memo <fretn655@gmail.com>
2026-04-08 17:03:39 -0600

    se cargan los 2 dataframes: companies and locations

--- proyectosprint8.py
+++ proyectosprint8.py
@@ -4,3 +4,8 @@
 import pandas as pd
 
 # --------------- Fin de las importaciones
+
+"""aqui se traen los dos primeros archivos excel para hacer data frame para analizarlos."""
+
+df_companies = pd.read_csv("/datasets/project_sql_result_01.csv")
+df_locations = pd.read_csv("/datasets/project_sql_result_04.csv")
